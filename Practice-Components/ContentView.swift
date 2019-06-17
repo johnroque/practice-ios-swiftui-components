73dd96ed90c54ebf49ea2548dc16bf168c4baf33
John Roque Jorillo <jorillojohnroque@gmail.com>
2019-06-17 15:51:37 +0800

    Initial Commit

---
+++ Practice-Components/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Practice-Components
+//
+//  Created by John Roque Jorillo on 6/17/19.
+//  Copyright © 2019 John Roque Jorillo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
